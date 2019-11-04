1e22d7a6c3344cac67be3051d9e73e91ce9cb6cf
Manuel Suess <adamnikova@gmail.com>
2019-11-04 11:11:18 +0100

    added StudyBuddy Xcode Project

---
+++ StudyBuddy/StudyBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudyBuddy
+//
+//  Created by Manuel Suess on 04.11.19.
+//  Copyright © 2019 piose5. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
