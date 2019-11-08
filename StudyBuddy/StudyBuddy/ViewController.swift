67cf4d3f44bde9ee4328e88c39295b7f1033cc95
Manuel <adamnikova@gmail.com>
2019-11-08 10:18:31 +0100

    added new (empty) StudyBuddy Project

---
+++ StudyBuddy/StudyBuddy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyBuddy
+//
+//  Created by Manuel Suess on 08.11.19.
+//  Copyright © 2019 Manuel Suess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
