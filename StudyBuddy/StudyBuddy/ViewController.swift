02444ee69e799cc2ffae5894f7893bcc2380d7eb
Liliane Kabboura <kabboura@cip.ifi.lmu.de>
2019-11-11 12:31:35 +0100

    Constrains added and Login, signup button  changed

--- StudyBuddy/StudyBuddy/ViewController.swift
+++ StudyBuddy/StudyBuddy/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var EnterUsernameAndPasswordLabel: UILabel!
+    @IBOutlet weak var UsernameTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var OrLabel: UILabel!
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    @IBOutlet weak var SignupButton: UIButton!
     override func viewDidLoad() {
+     
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        SetupUI()
     }
+    
+    func SetupUI() {
+            LoginButton.setTitle("Log In", for: UIControl.State.normal)
+            LoginButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+            LoginButton.layer.cornerRadius = 5
+            LoginButton.clipsToBounds = true
+        
+        SignupButton.setTitle("Sign Up", for: UIControl.State.normal)
+        SignupButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        SignupButton.layer.cornerRadius = 5
+        SignupButton.clipsToBounds = true
+        
+        }
+    
+        
+      
+        }
+        
+
+    
+
+        
+        
+    
 
 
-}
+
 
