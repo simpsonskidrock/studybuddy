f32f73af45dfbd8d3e5c6defb86d6530d2a9b736
Annika Jung <Annika.Jung@maibornwolff.de>
2019-11-13 12:46:45 +0100

    Added Swift UI Project

---
+++ StudyBuddySwiftUi/StudyBuddySwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StudyBuddySwiftUi
+//
+//  Created by Annika Jung on 13.11.19.
+//  Copyright © 2019 Annika Jung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
