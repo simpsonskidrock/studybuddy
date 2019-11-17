990861fd369518ad567e61fd97cc5e6883b5c349
Liliane Kabboura <kabboura@cip.ifi.lmu.de>
2019-11-17 22:41:22 +0100

    modified change password view

--- StudyBuddySwiftUi/StudyBuddySwiftUi/Reset Password/ChangePasswordView.swift
+++ StudyBuddySwiftUi/StudyBuddySwiftUi/Reset Password/ChangePasswordView.swift
@@ -1,66 +1,21 @@
 //
-//  RegisterView.swift
+//  ChangePasswordView.swift
 //  StudyBuddySwiftUi
 //
-//  Created by Annika Jung on 14.11.19.
+//  Created by Liliane Kabboura on 17.11.19.
 //  Copyright © 2019 Annika Jung. All rights reserved.
 //
 
-import Foundation
 import SwiftUI
 
-struct RegisterView: View {
-    @State var resetPass = false
-
-    @State private var newPassword: String = ""
-    @State private var confirmPassword: String = ""
-
+struct ChangePasswordView: View {
     var body: some View {
-        ZStack {
-          VStack(spacing: 16) {
-               Text("Change Password").font(.largeTitle)
-                .foregroundColor(.lmuLightGrey)
-                Spacer()
-                
-                   
-            VStack {
-                Text("Create a new Password").foregroundColor(Color.white)
-            
-                TextField("New password", text: $newPassword)
-                .textFieldStyle(StudyTextFieldStyle())
-                .padding(.horizontal, 50)
-                TextField("Confirm your password", text: $confirmPassword)
-            .textFieldStyle(StudyTextFieldStyle())
-                .padding(.horizontal, 50)
-               
-            }
-                    Button(action: {
-                       self.registered.toggle()
-                    }) {
-                        Text("Reset Password")
-                            .font(.system(size: 20))
-                            .fontWeight(.heavy)
-                       }.buttonStyle(StudyButtonStyle())
-                      .sheet(isPresented: $resertPass) {
-                        GeneralTabView()
-            }
-           
-            
-            Spacer()
-                    Spacer()
-                
-                Spacer()
-            }
-            .padding(.horizontal)
-          .background(Color.lmuGreen.edgesIgnoringSafeArea(.vertical))
-        }
-        }
-        
-}
-
-struct RegisterView_Previews: PreviewProvider {
-    static var previews: some View {
-        RegisterView()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+struct ChangePasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChangePasswordView()
+    }
+}
