f7add0226cfb24a830ad49528dd8c92bad647427
Manuel Suess <adamnikova@gmail.com>
2019-11-20 12:48:25 +0100

    somehow apparently added a working Firebase Login/Signup

---
+++ StudyBuddySwiftUi/StudyBuddySwiftUi/SessionStore.swift
@@ -0,0 +1,67 @@
+//
+//  SessionStore.swift
+//  StudyBuddySwiftUi
+//
+//  Created by Manuel Suess on 20.11.19.
+//  Copyright © 2019 Annika Jung. All rights reserved.
+//
+
+import SwiftUI
+import Firebase
+import Combine
+
+class SessionStore : ObservableObject {
+    var didChange = PassthroughSubject<SessionStore, Never>()
+    var session: User? { didSet { self.didChange.send(self) }}
+    var handle: AuthStateDidChangeListenerHandle?
+
+    func listen () {
+        // monitor authentication changes using firebase
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            if let user = user {
+                // if we have a user, create a new user model
+                print("Got user: \(user)")
+                self.session = User(
+                    uid: user.uid,
+                    displayName: user.displayName, email: nil
+                )
+            } else {
+                // if we don't have a user, set our session to nil
+                self.session = nil
+            }
+        }
+    }
+
+    func signUp(
+        email: String,
+        password: String,
+        handler: @escaping AuthDataResultCallback
+        ) {
+        Auth.auth().createUser(withEmail: email, password: password, completion: handler)
+    }
+
+    func signIn(
+        email: String,
+        password: String,
+        handler: @escaping AuthDataResultCallback
+        ) {
+        Auth.auth().signIn(withEmail: email, password: password, completion: handler)
+    }
+
+    func signOut () -> Bool {
+        do {
+            try Auth.auth().signOut()
+            self.session = nil
+            return true
+        } catch {
+            return false
+        }
+    }
+    
+    
+    func unbind () {
+        if let handle = handle {
+            Auth.auth().removeStateDidChangeListener(handle)
+        }
+    }// additional methods (sign up, sign in) will go here
+}
