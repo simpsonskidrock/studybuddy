b3247967b3187ea85f1abe8539ce006bc3f24d4f
Annika Jung <Annika.Jung@maibornwolff.de>
2019-11-21 07:07:25 +0100

    added contactstab with chat - non-functional

---
+++ StudyBuddySwiftUi/StudyBuddySwiftUi/Chat/ChatView.swift
@@ -0,0 +1,21 @@
+//
+//  ChatView.swift
+//  StudyBuddySwiftUi
+//
+//  Created by Annika Jung on 20.11.19.
+//  Copyright © 2019 Annika Jung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView()
+    }
+}
--- StudyBuddySwiftUi/StudyBuddySwiftUi/TabBar/ContactsTabView.swift
+++ StudyBuddySwiftUi/StudyBuddySwiftUi/TabBar/ContactsTabView.swift
@@ -10,8 +10,35 @@ import Foundation
 import SwiftUI
 
 struct ContactsTabView: View {
+    @State var chatSelected = false
+    @State var showChat = false
+    
     var body: some View {
-        Text("Hello ContactsTab")
+        VStack {
+            Spacer()
+            Text("Chats")
+            Spacer()
+            List {
+                HStack {
+                    Text("Person1")
+                    Spacer()
+                    Button(action: {
+                        self.chatSelected.toggle()
+                        
+                    }) {
+                        Image(systemName: "bubble.left.and.bubble.right")
+                            .font(.system(size: 20))
+                    }
+                    .sheet(isPresented: $showChat) {
+                        ChatView()
+                    }
+                    .foregroundColor(.lmuLightGrey)
+                }
+                Text("Person2")
+                Text("Person3")
+            }
+            Spacer()
+        }
     }
 }
 
