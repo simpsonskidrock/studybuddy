6a22878562d0217b7f3e5132df2219559f1670ea
Liliane Kabboura <kabboura@cip.ifi.lmu.de>
2019-11-22 23:56:51 +0100

    removed the changes that were made to the SearchTabView and added a search TextField to the ContactsTabView

--- StudyBuddySwiftUi/StudyBuddySwiftUi/TabBar/ContactsTabView.swift
+++ StudyBuddySwiftUi/StudyBuddySwiftUi/TabBar/ContactsTabView.swift
@@ -10,46 +10,76 @@ import Foundation
 import SwiftUI
 
 struct ContactsTabView: View {
-    @State var showChat = false
+    @State private var searchText = ""
+    @State private var showCancelButton: Bool = false
+    
     
     var body: some View {
+        NavigationView {
         VStack {
-            Spacer()
-            Text("Chats")
-            Spacer()
-            List {
-                HStack {
-                    Text("Person1")
-                    Spacer()
+            HStack {
+                HStack{
+                    Image(systemName: "magnifyingglass")
+                    TextField("search", text: $searchText, onEditingChanged: { isEditing in
+                        self.showCancelButton = true
+                    }, onCommit: {
+                        print("onCommit")
+                    }).foregroundColor(.primary)
                     Button(action: {
-                        self.showChat.toggle()
+                        self.searchText = ""
                     }) {
-                        Image(systemName: "bubble.left.and.bubble.right")
-                            .font(.system(size: 20))
+                        Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1)
                     }
-                    .sheet(isPresented: $showChat) {
-                        ChatView()
-                    }
-                    .foregroundColor(.lmuLightGrey)
                 }
-                HStack {
-                    Text("Person2")
-                    Spacer()
-                    Button(action: {
-                        self.showChat.toggle()
-                    }) {
-                        Image(systemName: "bubble.left.and.bubble.right")
-                            .font(.system(size: 20))
+                .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+                .foregroundColor(.secondary)
+                .background(Color(.secondarySystemBackground))
+                .cornerRadius(10.0)
+                if showCancelButton  {
+                    Button("Cancel") {
+                        self.searchText = ""
+                        self.showCancelButton = false
                     }
-                    .sheet(isPresented: $showChat) {
-                        ChatView()
-                    }
-                    .foregroundColor(.lmuLightGrey)
+                    .foregroundColor(Color(.systemBlue))
                 }
             }
-            Spacer()
-        }.navigationBarTitle("Chats")
+            .padding(.horizontal)
+            .navigationBarHidden(showCancelButton)
+            List() {
+             /*
+                 Filtered list of names by search (Todo)
+             ForEach(array.filter{$0.hasPrefix(searchText) || searchText == ""}, id:\.self) {
+                 searchText in Text(searchText)
+                                                */
+                HStack {
+                    NavigationLink(destination: ChatView()){
+                        Text("Person1")
+                    }
+                    Spacer()
+                        Image(systemName: "bubble.left.and.bubble.right")
+                            .font(.system(size: 20))
+                    .foregroundColor(.lmuLightGrey)
+                }
+                
+               HStack {
+                   NavigationLink(destination: ChatView()){
+                       Text("Person2")
+                   }
+                   Spacer()
+                       Image(systemName: "bubble.left.and.bubble.right")
+                           .font(.system(size: 20))
+                   .foregroundColor(.lmuLightGrey)
+               }
+                
+                
+            } .navigationBarTitle(Text("Chats"))
+            .resignKeyboardOnDragGesture()
+
+           
+        }
+
     }
+}
 }
 
 struct ContactsTabView_Previews: PreviewProvider {
@@ -57,3 +87,28 @@ struct ContactsTabView_Previews: PreviewProvider {
         ContactsTabView()
     }
 }
+extension UIApplication {
+    func endEditing(_ force: Bool) {
+        self.windows
+            .filter{$0.isKeyWindow}
+            .first?
+            .endEditing(force)
+    }
+}
+
+struct ResignKeyboardOnDragGesture: ViewModifier {
+    var gesture = DragGesture().onChanged{_ in
+        UIApplication.shared.endEditing(true)
+    }
+    func body(content: Content) -> some View {
+        content.gesture(gesture)
+    }
+}
+
+extension View {
+    func resignKeyboardOnDragGesture() -> some View {
+        return modifier(ResignKeyboardOnDragGesture())
+        
+    }
+}
+
--- StudyBuddySwiftUi/StudyBuddySwiftUi/TabBar/SearchTabView.swift
+++ StudyBuddySwiftUi/StudyBuddySwiftUi/TabBar/SearchTabView.swift
@@ -10,53 +10,10 @@ import Foundation
 import SwiftUI
 
 struct SearchTabView: View {
-    // i created this array only to check the layout, later on here the data from our data base going to be listed
-    let array = ["Peter", "Paul", "Mary", "Anna-Lena", "George", "John", "Greg", "Thomas", "Robert", "Bernie", "Mike", "Benno", "Hugo", "Miles", "Michael", "Mikel", "Tim", "Tom", "Lottie", "Lorrie", "Barbara"]
-    
-    @State private var searchText = ""
-    @State private var showCancelButton: Bool = false
+  
     var body: some View {
-        NavigationView {
-            VStack {
-                HStack {
-                    HStack{
-                        Image(systemName: "magnifyingglass")
-                        TextField("search", text: $searchText, onEditingChanged: { isEditing in
-                            self.showCancelButton = true
-                        }, onCommit: {
-                            print("onCommit")
-                        }).foregroundColor(.primary)
-                        Button(action: {
-                            self.searchText = ""
-                        }) {
-                            Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1)
-                        }
-                    }
-                    .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
-                    .foregroundColor(.secondary)
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(10.0)
-                    if showCancelButton  {
-                        Button("Cancel") {
-                            self.searchText = ""
-                            self.showCancelButton = false
-                        }
-                        .foregroundColor(Color(.systemBlue))
-                    }
-                }
-                .padding(.horizontal)
-                .navigationBarHidden(showCancelButton)
-                List {
-                    // Filtered list of names
-                    ForEach(array.filter{$0.hasPrefix(searchText) || searchText == ""}, id:\.self) {
-                        searchText in Text(searchText)
-                    }
-                }
-                .navigationBarTitle(Text("Search"))
-                .resignKeyboardOnDragGesture()
-            }
-        }
-    }
+       Text("Hallo")
+}
 }
 
 struct SearchTabView_Previews: PreviewProvider {
@@ -70,26 +27,3 @@ struct SearchTabView_Previews: PreviewProvider {
     }
 }
 
-extension UIApplication {
-    func endEditing(_ force: Bool) {
-        self.windows
-            .filter{$0.isKeyWindow}
-            .first?
-            .endEditing(force)
-    }
-}
-
-struct ResignKeyboardOnDragGesture: ViewModifier {
-    var gesture = DragGesture().onChanged{_ in
-        UIApplication.shared.endEditing(true)
-    }
-    func body(content: Content) -> some View {
-        content.gesture(gesture)
-    }
-}
-
-extension View {
-    func resignKeyboardOnDragGesture() -> some View {
-        return modifier(ResignKeyboardOnDragGesture())
-    }
-}
