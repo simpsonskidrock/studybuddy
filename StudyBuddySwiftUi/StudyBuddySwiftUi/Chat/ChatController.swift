575c211797bc855070953355fb19ace013c835af
Liliane Kabboura <kabboura@cip.ifi.lmu.de>
2019-11-23 16:31:33 +0100

    modified ChatView

---
+++ StudyBuddySwiftUi/StudyBuddySwiftUi/Chat/ChatController.swift
@@ -0,0 +1,28 @@
+//
+//  ChatController.swift
+//  StudyBuddySwiftUi
+//
+//  Created by Liliane Kabboura on 23.11.19.
+//  Copyright © 2019 Annika Jung. All rights reserved.
+//
+import Combine
+import SwiftUI
+import Foundation
+
+class ChatController : ObservableObject {
+  
+    var didChange = PassthroughSubject<Void, Never>()
+    
+    
+    @Published var messages = [
+        ChatMessage(message: "Hello world", avatar: "A", color: .lmuDarkGrey),
+    ]
+    
+  
+    func sendMessage(_ chatMessage: ChatMessage) {
+        messages.append(chatMessage)
+        
+        didChange.send(())
+    }
+    
+}
--- StudyBuddySwiftUi/StudyBuddySwiftUi/Chat/ChatView.swift
+++ StudyBuddySwiftUi/StudyBuddySwiftUi/Chat/ChatView.swift
@@ -7,15 +7,94 @@
 //
 
 import SwiftUI
+import Foundation
 
-struct ChatView: View {
+
+struct ChatMessage : Hashable {
+    var message: String
+    var avatar: String
+    var color: Color
+    // isMe will be true if We sent the message
+    var isMe: Bool = false
+}
+
+struct ChatView : View {
+    
+    @State var composedMessage: String = ""
+    @EnvironmentObject var chatController: ChatController
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        
+        VStack {
+            Text("Chat").bold()
+            List {
+                ForEach(chatController.messages, id: \.self) { msg in
+                    ChatRow(chatMessage: msg)
+                }
+            }
+            
+            
+            HStack {
+                
+                TextField("Message...", text: $composedMessage).frame(minHeight: CGFloat(30))
+                    .textFieldStyle(StudyTextFieldStyle())
+                
+                Button(action: sendMessage) {
+                    Text("Send")
+                        .foregroundColor(.black)
+                }
+            }.frame(minHeight: CGFloat(50)).padding()
+                .background(Color.lmuGreen)
+            
+            
+        }.navigationBarTitle(Text("Chats"))
+        
+    }
+    func sendMessage() {
+        chatController.sendMessage(ChatMessage(message: composedMessage, avatar: "B", color: .lmuLightGrey, isMe: true))
+        composedMessage = ""
     }
 }
 
-struct ChatView_Previews: PreviewProvider {
+
+struct ChatRow : View {
+    var chatMessage: ChatMessage
+    var body: some View {
+        Group {
+            if !chatMessage.isMe {
+                HStack {
+                    Group {
+                        Text(chatMessage.avatar)
+                        Text(chatMessage.message)
+                            .bold()
+                            .padding(10)
+                            .foregroundColor(Color.white)
+                            .background(chatMessage.color)
+                            .cornerRadius(10)
+                    }
+                }
+            } else {
+                HStack {
+                    Group {
+                        Spacer()
+                        Text(chatMessage.message)
+                            .bold()
+                            .foregroundColor(Color.white)
+                            .padding(10)
+                            .background(chatMessage.color)
+                            .cornerRadius(10)
+                        Text(chatMessage.avatar)
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+struct ChatView_Previews : PreviewProvider {
     static var previews: some View {
-        ChatView()
+        ChatView().environmentObject(ChatController())
     }
 }
