831d7d1c696d67f66005ae3f5aa1c0af7b17f07c
Manuel Suess <adamnikova@gmail.com>
2019-12-17 13:30:01 +0100

    added stupid little text that appears once you double tap in de swipe-view

--- StudyBuddySwiftUi/StudyBuddySwiftUi/Models/otherUsersPresentation/OtherUserView.swift
+++ StudyBuddySwiftUi/StudyBuddySwiftUi/Models/otherUsersPresentation/OtherUserView.swift
@@ -10,18 +10,41 @@ import SwiftUI
 
 struct OtherUserView: View {
     @EnvironmentObject var session: CommunicationStore
-    
+    @State private var showText = false
     let userModel: UserModel
     
+   
+    
     var body: some View {
+        
+       
+        
         ZStack(alignment: .leading) {
+           
             OtherUserAvatarView(userModel: userModel)
             OtherUserNameView(name: userModel.displayName!, fieldOfStudy: userModel.fieldOfStudy!, description: userModel.description!, hashtags: userModel.hashtags!)
+        
+        if showText {
+            Text("Like")
+                .background(Color.lmuDarkGrey)
+                .cornerRadius(10.0)
+                .foregroundColor(Color.white)
+                .font(.largeTitle)
+                
+                
+                    }
+        
         }
         .shadow(radius: 12.0)
         .cornerRadius(12.0)
         .onTapGesture(count: 2) {
             self.session.addLikedUser(uid: self.userModel.uid)
+            
+            withAnimation(.spring()){
+            self.showText.toggle()
+            }
+            
+            }
         }
     }
-}
+
