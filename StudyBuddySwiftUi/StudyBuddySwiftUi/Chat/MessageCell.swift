aa2688a9ce60488b3d54e7995a09d4869c9d0043
Liliane Kabboura <kabboura@cip.ifi.lmu.de>
2019-12-18 09:48:20 +0100

    added MessageCell.xib

---
+++ StudyBuddySwiftUi/StudyBuddySwiftUi/Chat/MessageCell.swift
@@ -0,0 +1,27 @@
+//
+//  MessageCell.swift
+//  StudyBuddySwiftUi
+//
+//  Created by Liliane Kabboura on 17.12.19.
+//  Copyright © 2019 Annika Jung. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var MessageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
