1b92cc4ad7fce8e09803bbf3231432b093a0dfc8
Annika Jung <Annika.Jung@maibornwolff.de>
2019-12-19 10:54:51 +0100

    functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,3 @@
-/* Start writing Firebase Functions
-** https://firebase.google.com/docs/functions/typescript
-*/
-
-
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
@@ -10,17 +5,10 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/Users/{userId}')
-    .onCreate((snapshot, context) => {
-        // Grab the current value of what was written to the Realtime Database.
-        const original = snapshot.val();
-        console.log('UserID', context.params.userId, original);
-        // console.log('Uppercasing', context.params.pushId, original);
-        //const uppercase = original.toUpperCase();
-        // You must return a Promise when performing asynchronous tasks inside a Functions such as
-        // writing to the Firebase Realtime Database.
-        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-        //return snapshot.ref.parent.child('uppercase').set(uppercase);
-    });
+exports.newLike = functions.database.ref('/Users/{userId}/likedUsers')
+    .onWrite((change: any, context: any) => {
+        const original = change.after.val()
+        console.log("LowerCasing", context.params.userId, original)
+        const lowercase = original.toLowerCase()
+        return change.afer.ref.parent.child('lowercase').set(lowercase)
+    })
