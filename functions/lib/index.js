c02da6deb7a04a173fa669c1f36819301890e843
Annika Jung <Annika.Jung@maibornwolff.de>
2019-12-19 11:00:57 +0100

    merge

---
+++ functions/lib/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+exports.newLike = functions.database.ref('/Users/{userId}/likedUsers')
+    .onWrite((change, context) => {
+    const original = change.after.val();
+    console.log("LowerCasing", context.params.userId, original);
+    const lowercase = original.toLowerCase();
+    return change.afer.ref.parent.child('lowercase').set(lowercase);
+});
+//# sourceMappingURL=index.js.map
